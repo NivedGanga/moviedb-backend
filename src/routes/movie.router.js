0be51713945d1d1ab104d075495b2d9f131bb746
NivedGanga <nivedganga@gmail.com>
2024-06-07 02:09:05 +0530

    post method to add movie datas

--- src/routes/movie.router.js
+++ src/routes/movie.router.js
@@ -1,7 +1,12 @@
 const { addMovie, getMovies, searchMovies } = require("../controller/movie.controller")
 const router = require("express").Router()
+//addmovies
+router.post("/", addMovie)
+
 // for search a movie by its name
-router.post("/", searchMovies)
+//router.post("/", searchMovies)
+
 //for get list of movies
 router.get("/", getMovies)
+
 module.exports = router;
