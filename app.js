e28b32f550e91fb6e95f9d2aec1fd3dd35edc3cd
NivedGanga <nivedganga@gmail.com>
2024-06-07 13:41:02 +0530

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 require('dotenv').config()
 const movieRouter = require("./src/routes/movie.router")
-
+const cors = require('cors');
 //creating app
 const app = express()
 
 app.use(express.json())
-
+app.use(cors());
 const port = process.env.PORT || 10000
 
 //routes
